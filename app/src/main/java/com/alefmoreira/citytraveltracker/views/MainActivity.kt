357589bc2fbdf56e40dd89c1514c30d30978a154
Alef Moreira <alefmoreira@brq.com>
2023-07-03 16:49:10 -0300

    SplashScreen

--- app/src/main/java/com/alefmoreira/citytraveltracker/views/MainActivity.kt
+++ app/src/main/java/com/alefmoreira/citytraveltracker/views/MainActivity.kt
@@ -2,17 +2,18 @@ package com.alefmoreira.citytraveltracker.views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.alefmoreira.citytraveltracker.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.alefmoreira.citytraveltracker.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
     }
 }
